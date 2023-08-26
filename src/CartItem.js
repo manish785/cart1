3e4c397ee711df2ee74cee6ea3f21171407b36f1
manish785 <kumarachilish1997@gmail.com>
2023-08-26 10:42:30 +0530

    feat : Implemented IncreaseQuantity Functionality

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,34 +1,63 @@
-import React from "react";
-
+import React from 'react';
 
 class CartItem extends React.Component {
-    render () {
-       return (
-        <div className="cart-item">
-            <div className="left-block">
-                <img style={styles.image}/>
-            </div>
-
-            <div className="right-block">
-                <div style={ {fontSize: 25} }>Phone</div>
-                <div style={ {color: '#777'} }>RS 999</div>
-                <div style={ {color: '#777'} }>Qty: 1</div>
-            </div>
-            <div className="cart-item-actions">
-                 {/*Button*/}
-            </div>
-        </div>
-       );
+  constructor () {
+    super();
+    this.state = {
+      price: 999,
+      title: 'Mobile Phone',
+      qty: 1,
+      img: ''
     }
+    // this.increaseQuantity = this.increaseQuantity.bind(this);
+  }
+
+  increaseQuantity = () => {
+      console.log('this', this.state);
+  }
+  render () {
+    const { price, title, qty } = this.state;
+    return (
+      <div className="cart-item">
+        <div className="left-block">
+          <img style={styles.image} />
+        </div>
+        <div className="right-block">
+          <div style={ { fontSize: 25 } }>{title}</div>
+          <div style={ { color: '#777' } }>Rs {price} </div>
+          <div style={ { color: '#777' } }>Qty: {qty} </div>
+          <div className="cart-item-actions">
+            {/* Buttons */}
+            <img
+              alt="increase"
+              className="action-icons"
+              src="https://as2.ftcdn.net/v2/jpg/01/26/10/59/1000_F_126105961_6vHCTRX2cPOnQTBvx9OSAwRUapYTEmYA.jpg"
+              onClick={this.increaseQuantity}
+            />
+            <img
+              alt="decrease"
+              className="action-icons"
+              src="https://as1.ftcdn.net/v2/jpg/03/73/49/86/1000_F_373498649_nBxauQ0ipBSVrVcMpWWVmTpXu3BLvRyY.jpg" 
+            />
+            <img
+              alt="delete"
+              className="action-icons"
+              src="https://as2.ftcdn.net/v2/jpg/01/90/89/15/1000_F_190891550_N7uKp2aHE3mOc20dmtDytj7atgvbhdOu.jpg"
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 const styles = {
-    image :{
-        height : 110,
-        width : 110,
-        borderRadius : 4,
-        background : '#ccc',
-    }
+  image: {
+    height: 110,
+    width: 110,
+    borderRadius: 4,
+    background: '#ccc'
+  }
 }
 
 export default CartItem;
