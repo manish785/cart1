00df62bf5f59775f202ff0253dbfe08586d4b57c
manish785 <kumarachilish1997@gmail.com>
2023-09-03 19:21:45 +0530

    feat : added the firebase data in the onSnapshot

--- src/App.js
+++ src/App.js
@@ -19,17 +19,30 @@ class  App extends React.Component{
 
     componentDidMount() {
     
+    //   firestore
+    //   .collection("products")
+    //   .get()
+    //   .then(snapshot => {
+    //     const products = snapshot.docs.map(doc => {
+    //       const data = doc.data();
+    //       data["id"] = doc.id;
+    //       return data;
+    //     });
+    //     this.setState({ products: products, loading: false });
+    //   });
+    // }
+
       firestore
       .collection("products")
-      .get()
-      .then(snapshot => {
+      .onSnapshot((snapshot => {
         const products = snapshot.docs.map(doc => {
           const data = doc.data();
           data["id"] = doc.id;
           return data;
         });
         this.setState({ products: products, loading: false });
-      });
+      }))
+        
     }
     
 
